6f8c39eafbb0f84c9a3ab315c57a73cc0858a142
Oh-HyunYoung <gusdud2709@gmail.com>
2023-02-13 12:16:40 +0900

    xml - exlicit configuration #finish

--- container/src/main/java/com/douzone/container/videosystem/IronMan.java
+++ container/src/main/java/com/douzone/container/videosystem/IronMan.java
@@ -1,7 +1,7 @@
 package com.douzone.container.videosystem;
 
-public class Avengers implements DigitalVideoDisc {
-	private String title = "Avengers";
+public class IronMan implements DigitalVideoDisc {
+	private String title = "Iron Man";
 	private String studio = "MARVEL";
 	
 	@Override
@@ -9,5 +9,5 @@ public class Avengers implements DigitalVideoDisc {
 		return "Playing Movie " + studio +"'s " + title;
 	}
 
-	
 }
+
