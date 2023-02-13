88e6da5c10e0acb29903ee9c57004ef4baa5a66f
Oh-HyunYoung <gusdud2709@gmail.com>
2023-02-13 09:56:50 +0900

    explicit - xml bean configuration #1

--- container/src/main/java/com/douzone/container/soundsystem/CDPlayer.java
+++ container/src/main/java/com/douzone/container/soundsystem/CDPlayer.java
@@ -8,6 +8,12 @@ public class CDPlayer {
 	@Autowired
 	private CompactDisc cd;
 	
+	// 위의 Autowired 8-9줄 문법 대신 아래의 문법 사용 권장
+	// private의 Autowired는 권장되지 않음(보안상의 문제로..)
+	CDPlayer(CompactDisc cd){
+		this.cd = cd;
+	}
+	
 	public String play() {
 		return cd.play();
 	}
